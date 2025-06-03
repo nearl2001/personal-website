e0871dec5b33d5dfd806f420040ecac3cf0665a7
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-06-03 16:28:46 -0600

    feat(navbar): initial design

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AboutMeButtons } from "../about-me-buttons/about-me-buttons";
 
 @Component({
-  selector: 'app-navbar',
-  imports: [],
+  selector: 'navbar',
+  imports: [AboutMeButtons],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
