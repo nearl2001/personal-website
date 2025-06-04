346f5e9a99a49b19dd6b80fcad14552033158195
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-06-03 18:30:41 -0600

    feat(projects): added first iteration of projects menu

--- src/app/components/project-card/project-card.ts
+++ src/app/components/project-card/project-card.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+interface dataInput {
+  name: string;
+  description: string;
+  pic1Ref: string;
+  pic2Ref?: string;
+}
 
 @Component({
   selector: 'app-project-card',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './project-card.html',
   styleUrl: './project-card.css'
 })
 export class ProjectCard {
-
+  data = input<dataInput>({name: 'Unknown!', description: 'Not Passed Through!', pic1Ref: 'urmom.jpg', pic2Ref: ''})
 }
--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ProjectCard } from '../project-card/project-card';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [ProjectCard],
   templateUrl: './projects.html',
   styleUrl: './projects.css'
 })
 export class Projects {
+  @ViewChild('scrollContainer', { static: true }) scrollContainer!: ElementRef<HTMLDivElement>;
 
+  cards = [{pic1Ref: 'picRef.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', name: 'Dummy hehe'},
+           {pic1Ref: 'picRef.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', name: 'Dummy hehe'},
+           {pic1Ref: 'picRef.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', name: 'Dummy hehe'},
+           {pic1Ref: 'picRef.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', name: 'Dummy hehe'}
+          ];
+  activeIndex = 0;
+  
+  ngAfterViewInit() {
+    this.scrollContainer.nativeElement.addEventListener('scroll', this.onScroll.bind(this));
+  }
+  
+  onScroll() {
+    const container = this.scrollContainer.nativeElement;
+    const scrollLeft = container.scrollLeft;
+    const cardWidth = container.firstElementChild?.clientWidth || 1;
+    const index = Math.round(scrollLeft / (cardWidth + 16)); // +gap (space-x-4 = 1rem = 16px)
+    this.activeIndex = index;
+  }
 }
